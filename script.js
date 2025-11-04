ce8ced1ecfe16b41f2f97f0c7da0b031f2bc9184
M-Husni07 <husni12011980@gmail.com>
2025-11-04 22:56:12 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+window.addEventListener('scroll', () => {
+  document.querySelectorAll('.fade-scroll').forEach(el => {
+    const rect = el.getBoundingClientRect();
+    if (rect.top < window.innerHeight - 100) {
+      el.classList.add('visible');
+    }
+  });
+});
